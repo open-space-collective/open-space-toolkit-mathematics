d582463efbeb1643c78e54a200ab69ef66e57760
Lucas Brémond <lucas.bremond@gmail.com>
2018-08-21 12:18:44 -0700

    [misc] Improve Jupyter Notebook Docker setup

--- tools/python/start.sh
+++ tools/python/start.sh
@@ -24,20 +24,18 @@ if [[ ! -z $1 ]] && [[ $1 == "--link" ]]; then
     --publish="8887:8888" \
     --user="" \
     --env="JUPYTER_LAB_ENABLE=yes" \
-    --env="LD_LIBRARY_PATH=/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
+    --env="LD_LIBRARY_PATH=/usr/local/lib:/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
     --env="PYTHONPATH=/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
     --volume="$(pwd)/../../../library-core/lib:/opt/library-core:ro" \
     --volume="$(pwd)/../../lib:/opt/lib:ro" \
     --volume="$(pwd)/../../share/python/notebooks:/home/jovyan/notebooks" \
     --volume="$(pwd)/../../share/data:/app/share/data" \
-    jupyter/scipy-notebook \
+    "${repository_name}/${project_name}-python" \
     bash -c "mkdir -p /opt/conda/lib/python3.6/site-packages/Library/Core \
-    && ln -s /opt/library-core/libboost_python36.so.1.68.0 /opt/conda/lib/python3.6/site-packages/Library/Core/libboost_python36.so.1.68.0 \
     && ln -s /opt/library-core/liblibrary-core.so.0 /opt/conda/lib/python3.6/site-packages/Library/Core/liblibrary-core.so.0 \
     && ln -s /opt/library-core/LibraryCorePy.so /opt/conda/lib/python3.6/site-packages/Library/Core/LibraryCorePy.so \
     && echo 'from .LibraryCorePy import *' > /opt/conda/lib/python3.6/site-packages/Library/Core/__init__.py \
     && mkdir -p /opt/conda/lib/python3.6/site-packages/Library/Mathematics \
-    && ln -s /opt/lib/libboost_python36.so.1.68.0 /opt/conda/lib/python3.6/site-packages/Library/Mathematics/libboost_python36.so.1.68.0 \
     && ln -s /opt/lib/liblibrary-mathematics.so.0 /opt/conda/lib/python3.6/site-packages/Library/Mathematics/liblibrary-mathematics.so.0 \
     && ln -s /opt/lib/LibraryMathematicsPy.so /opt/conda/lib/python3.6/site-packages/Library/Mathematics/LibraryMathematicsPy.so \
     && echo 'from .LibraryMathematicsPy import *' > /opt/conda/lib/python3.6/site-packages/Library/Mathematics/__init__.py \
@@ -52,15 +50,14 @@ else
     --publish="8887:8888" \
     --user="" \
     --env="JUPYTER_LAB_ENABLE=yes" \
-    --env="LD_LIBRARY_PATH=/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
+    --env="LD_LIBRARY_PATH=/usr/local/lib:/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
     --env="PYTHONPATH=/opt/conda/lib/python3.6/site-packages:/home/jovyan/lib" \
     --volume="$(pwd)/../../../library-core/lib:/opt/library-core:ro" \
     --volume="$(pwd)/../../lib:/opt/lib:ro" \
     --volume="$(pwd)/../../share/python/notebooks:/home/jovyan/notebooks" \
     --volume="$(pwd)/../../share/data:/app/share/data" \
-    jupyter/scipy-notebook \
+    "${repository_name}/${project_name}-python" \
     bash -c "mkdir -p /opt/conda/lib/python3.6/site-packages/Library/Mathematics \
-    && ln -s /opt/lib/libboost_python36.so.1.68.0 /opt/conda/lib/python3.6/site-packages/Library/Mathematics/libboost_python36.so.1.68.0 \
     && ln -s /opt/lib/liblibrary-mathematics.so.0 /opt/conda/lib/python3.6/site-packages/Library/Mathematics/liblibrary-mathematics.so.0 \
     && ln -s /opt/lib/LibraryMathematicsPy.so /opt/conda/lib/python3.6/site-packages/Library/Mathematics/LibraryMathematicsPy.so \
     && echo 'from .LibraryMathematicsPy import *' > /opt/conda/lib/python3.6/site-packages/Library/Mathematics/__init__.py \
