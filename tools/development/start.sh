0983bfa4bf57e3fcd57a147b656d8fe7d4d0c4e8
Lucas Brémond <lucas.bremond@gmail.com>
2018-11-16 21:42:27 +0900

    [misc] Update Dockerfile

--- tools/development/start.sh
+++ tools/development/start.sh
@@ -17,7 +17,7 @@ source "${script_directory}/../.env"
 
 # Build Docker image if it does not exist already
 
-if [[ "$(docker images -q ${image_name} 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q ${image_name}:${image_version} 2> /dev/null)" == "" ]]; then
 
     pushd "${script_directory}/docker" > /dev/null
 
@@ -75,7 +75,6 @@ docker run \
 -it \
 --rm \
 --privileged \
---env="cpu_count=${cpu_count}" \
 ${options} \
 --volume="${project_directory}:/app:rw" \
 --volume="${script_directory}/helpers/build.sh:/app/build/build.sh:ro" \
@@ -83,7 +82,7 @@ ${options} \
 --volume="${script_directory}/helpers/debug.sh:/app/build/debug.sh:ro" \
 --volume="${script_directory}/helpers/clean.sh:/app/build/clean.sh:ro" \
 --workdir="/app/build" \
-"${image_name}" \
+"${image_name}:${image_version}" \
 /bin/bash -c "${command}"
 
 ################################################################################################################################################################
