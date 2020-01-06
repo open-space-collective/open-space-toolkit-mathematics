a80c9ca3c89c0d80ab3720fe2d4ed792ac641b04
Lucas Brémond <lucas.bremond@gmail.com>
2020-01-06 01:16:52 -0800

    [misc] Improve Python test

--- bindings/python/test/test_objects.py
+++ bindings/python/test/test_objects.py
@@ -7,6 +7,28 @@
 
 ################################################################################################################################################################
 
-# [TBI]
+import ostk.mathematics as mathematics
+
+################################################################################################################################################################
+
+Angle = mathematics.geometry.Angle
+
+Point2d = mathematics.geometry.d2.objects.Point
+Polygon2d = mathematics.geometry.d2.objects.Polygon
+Transformation2d = mathematics.geometry.d2.Transformation
+Point3d = mathematics.geometry.d3.objects.Point
+PointSet3d = mathematics.geometry.d3.objects.PointSet
+Line3d = mathematics.geometry.d3.objects.Line
+Ray3d = mathematics.geometry.d3.objects.Ray
+Segment3d = mathematics.geometry.d3.objects.Segment
+Plane = mathematics.geometry.d3.objects.Plane
+Polygon3d = mathematics.geometry.d3.objects.Polygon
+Cuboid = mathematics.geometry.d3.objects.Cuboid
+Sphere = mathematics.geometry.d3.objects.Sphere
+Ellipsoid = mathematics.geometry.d3.objects.Ellipsoid
+Transformation3d = mathematics.geometry.d3.Transformation
+Quaternion = mathematics.geometry.d3.transformations.rotations.Quaternion
+RotationVector = mathematics.geometry.d3.transformations.rotations.RotationVector
+RotationMatrix = mathematics.geometry.d3.transformations.rotations.RotationMatrix
 
 ################################################################################################################################################################
