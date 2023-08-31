57270d43899379259b89f97f579ffd809c770239
Vishwa Shah <vishwa2710@gmail.com>
2023-08-31 00:06:32 +0000

    feat: add Numerical Solver class from OSTk Astro

--- bindings/python/src/OpenSpaceToolkitMathematicsPy.cxx
+++ bindings/python/src/OpenSpaceToolkitMathematicsPy.cxx
@@ -1,15 +1,18 @@
 /// Apache License 2.0
-
+#define PYBIND11_DETAILED_ERROR_MESSAGES
 #include <OpenSpaceToolkitMathematicsPy/Utilities/ArrayCasting.hpp>
 #include <OpenSpaceToolkitMathematicsPy/Utilities/ShiftToString.hpp>
 #include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
 
 #include <OpenSpaceToolkitMathematicsPy/CurveFitting.cpp>
 #include <OpenSpaceToolkitMathematicsPy/Geometry.cpp>
 #include <OpenSpaceToolkitMathematicsPy/Objects.cpp>
+#include <OpenSpaceToolkitMathematicsPy/Solvers.cpp>
 
 PYBIND11_MODULE(OpenSpaceToolkitMathematicsPy, m)
 {
@@ -33,4 +36,5 @@ PYBIND11_MODULE(OpenSpaceToolkitMathematicsPy, m)
     OpenSpaceToolkitMathematicsPy_Objects(m);
     OpenSpaceToolkitMathematicsPy_Geometry(m);
     OpenSpaceToolkitMathematicsPy_CurveFitting(m);
+    OpenSpaceToolkitMathematicsPy_Solvers(m);
 }
